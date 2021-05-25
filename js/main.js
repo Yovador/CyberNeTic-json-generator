5e1501c864f1195505883d1ba8ba78daf91d1cc8
Yovador <yovador@gmail.com>
2021-05-25 15:56:53 +0200

    added unity version in readme

--- js/main.js
+++ js/main.js
@@ -1062,10 +1062,9 @@ const AddZoom = () => {
         //Désactive le zoom en double cliquant
         zoomDoubleClickSpeed: 1, 
         beforeMouseDown: function(e) {
-            
-          // allow mouse-down panning only if altKey is down. Otherwise - ignore
-          var shouldIgnore = !e.altKey;
-          return shouldIgnore;
+            // allow mouse-down panning only if altKey is down. Otherwise - ignore
+            var shouldIgnore = !e.altKey;
+            return shouldIgnore;
         }
       })
 }
