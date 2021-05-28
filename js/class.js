8a71ebd4f899a295f2717af19e39ad8ea2443b5f
Yovador <yovador@gmail.com>
2021-05-28 09:41:10 +0200

    panzoom et ajout des classes du characterSet

--- js/class.js
+++ js/class.js
@@ -6,8 +6,8 @@ class Content {
 }
 class Message {
     constructor(side, content, sendTime){
-        this.side = side,
-        this.content = content, 
+        this.side = side
+        this.content = content
         this.sendTime = sendTime
     }
 }
@@ -21,48 +21,28 @@ class BranchingPoint {
 
 class Branch{
     constructor(id, messagesList, branchingPoint){
-        this.id = id,
-        this.messagesList = messagesList,
+        this.id = id
+        this.messagesList = messagesList
         this.branchingPoint = branchingPoint
     }
 }
 
-const test = () =>{
-    let firstBranch = new Branch("1stbranch", [], null)
-    firstBranch.messagesList.push(new Message (true, new Content("text", "Salut"), 60) )
-    firstBranch.messagesList.push(new Message (false, new Content("image", "Ca va ?"), 65) )
-    firstBranch.branchingPoint = new BranchingPoint (
-        "choice", 
-        [
-            {branch:"1A",
-            possible:true,
-            confidenceMod:1,
-            message: new Message(false, new Content("text", "Oui et toi ?", 70))
-            },
+class Character{
+    constructor(id, profilePicture, firstName, lastName){
+        this.id =id;
+        this.profilePicture = profilePicture;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
 
-            {branch:"1B",
-            possible:true,
-            confidenceMod:-1,
-            message: new Message(false, new Content("text", "Non *dab*", 70))
-            }
-
-        ]) 
-
-    let oneA = new Branch("1A", [], null)
-    oneA.messagesList.push(new Message (true, new Content("text", "Cava, Cool !"), 75) )
-    oneA.branchingPoint = new BranchingPoint ( "stop", []) 
-    
-    let oneB = new Branch("1B", [], null)
-    oneB.messagesList.push(new Message (true, new Content("text", " cringe"), 75) )
-    oneB.branchingPoint = new BranchingPoint ( "stop", []) 
-
-    let allBranches = []
-    allBranches.push(firstBranch, oneA, oneB)
-
- 
-    return allBranches
+class Relationship{
+    constructor(me, them, confidenceMeToThem){
+        this.me = me
+        this.them = them
+        this.confidenceMeToThem = confidenceMeToThem
+    }
 }
 
 
-
-export {Content, Message, BranchingPoint, Branch}
+export {Content, Message, BranchingPoint, Branch, Character,Relationship}
