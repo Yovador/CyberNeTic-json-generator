a0393468bc4f9923e1dcd045b248392072a4c0d1
Yovador <yovador@gmail.com>
2021-05-28 09:33:07 +0200

    fix bouton dl

--- js/exportJson.js
+++ js/exportJson.js
@@ -1,4 +1,4 @@
-const ExportJson = (obj) => {
+const ExportJson = (obj, fileName) => {
 
 
     var dlAnchorElem = document.getElementById('downloadAnchorElem');
@@ -7,12 +7,12 @@ const ExportJson = (obj) => {
     
         console.log("Exporting...")
         console.log(obj)
-        let filename = obj.Conversation.id + ".json"
-        console.log(filename)
+        let fileNameFull = fileName + ".json"
+        console.log(fileNameFull)
         var json = JSON.stringify(obj);
         var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(json);
         dlAnchorElem.setAttribute("href",     dataStr     );
-        dlAnchorElem.setAttribute("download", filename);
+        dlAnchorElem.setAttribute("download", fileNameFull);
     }
     
     dlAnchorElem.addEventListener('click',function() {ExportFile(obj)} )
