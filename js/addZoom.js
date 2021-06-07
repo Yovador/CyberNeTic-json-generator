c7f05a5e7ce3cdae7f3f2495a4fe34bff506ec55
0ur0boss <enzoduret@yahoo.fr>
2021-06-07 11:32:41 +0200

    warningfirefox

--- js/addZoom.js
+++ js/addZoom.js
@@ -1,13 +1,21 @@
-const AddZoom = (div) => {
-    panzoom(div, {
-        //Désactive le zoom en double cliquant
-        zoomDoubleClickSpeed: 1, 
-        beforeMouseDown: function(e) {
-            // allow mouse-down panning only if altKey is down. Otherwise - ignore
-            var shouldIgnore = !e.altKey;
-            return shouldIgnore;
+    const AddZoom = (div) => {
+        try
+        {
+            panzoom(div, {
+                //Désactive le zoom en double cliquant
+                zoomDoubleClickSpeed: 1, 
+                beforeMouseDown: function(e) {
+                    // allow mouse-down panning only if altKey is down. Otherwise - ignore
+                    var shouldIgnore = !e.altKey;
+                    return shouldIgnore;
+                }
+            })
         }
-      })
-}
+        catch{
+        alert("Votre navigateur web ne supporte pas notre site web, veillez soit à le mettre à jour, soit à changer de navigateur (chrome, firefox ..).");
+        }
+    }
+
+
 
 export default AddZoom;
